fbb21e7b5e025a8828a613212556725e9c10e458
Ahli-baba <argiematondo@gmail.com>
2025-11-24 23:14:58 +0800

    11 - E-Commerce Project

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -15,9 +15,13 @@
 </head>
 
 <body class="bg-slate-200 dark:bg-slate-700">
+
+    @livewire('partials.navbar')
+
     <main>
         {{ $slot }}
     </main>
+    @livewire('partials.footer')
 
     {{-- Livewire JS --}}
     @livewireScripts
