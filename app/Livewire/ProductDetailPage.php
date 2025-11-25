265ec2299da85d49f557734e49b2af63dd7d397d
Ahli-baba <argiematondo@gmail.com>
2025-11-26 00:50:19 +0800

    17 -- E-Commerce Project

--- app/Livewire/ProductDetailPage.php
+++ app/Livewire/ProductDetailPage.php
@@ -5,18 +5,48 @@ namespace App\Livewire;
 use Livewire\Component;
 use Livewire\Attributes\Title;
 use App\Models\Product;
+use Jantinnerezo\LivewireAlert\Concerns\SweetAlert2;
+use App\Helpers\CartManagement;
+use App\Livewire\Partials\Navbar;
 
 #[Title('Product Detail Page - Matondo I Dagupan - CodeMania')]
 class ProductDetailPage extends Component
 {
-    public $slug;
+    use SweetAlert2;
 
+    public $slug;
+    public $quantity = 1;
     public function mount($slug)
     {
         $this->slug = $slug;
     }
 
+    public function increaseQty()
+    {
+        $this->quantity++;
+    }
 
+    public function decreaseQty()
+    {
+        if ($this->quantity > 1) {
+            $this->quantity--;
+        }
+    }
+
+    public function addToCart($product_id): void
+    {
+        $total_count = CartManagement::addItemToCart(product_id: $product_id);
+
+        $this->dispatch('update-cart-count', total_count: $total_count)
+            ->to(Navbar::class);
+
+        $this->dispatch('alert', [
+            'text' => 'Product added to the cart successfully!',
+            'toast' => true,
+            'position' => 'bottom-end',
+            'timer' => 3000,
+        ]);
+    }
 
     public function render()
     {
